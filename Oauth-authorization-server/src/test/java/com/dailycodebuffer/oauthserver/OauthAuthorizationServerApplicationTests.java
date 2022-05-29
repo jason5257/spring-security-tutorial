18a8f19fe397fec0249d7e4e41b148b66cd61371
jason <danmoogi2000@gmail.com>
2022-05-29 22:13:19 +0900

    중간

--- Oauth-authorization-server/src/test/java/com/dailycodebuffer/oauthserver/OauthAuthorizationServerApplicationTests.java
+++ Oauth-authorization-server/src/test/java/com/dailycodebuffer/oauthserver/OauthAuthorizationServerApplicationTests.java
@@ -2,12 +2,15 @@ package com.dailycodebuffer.oauthserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class OauthAuthorizationServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		System.out.println("(new BCryptPasswordEncoder(11)).encode(\"1234\") = " + (new BCryptPasswordEncoder(11)).encode("1234"));
 	}
 
 }
